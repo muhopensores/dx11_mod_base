5405698c1d4da25354747a40397a3f8f072b365a
Mstislav Kapustka <awesome065@yandex.com>
2021-01-08 22:00:46 +0400

    Update cmakelists.txt

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,5 +1,5 @@
 #define LOG_FILENAME "DMC2_framework_log.txt"
 #define LOG_ENTRY "DMC2 ModFramework entry"
 #define CONFIG_FILENAME "DMC2_fw_config.txt"
-#define GIT_HASH "d3463c5"
+#define GIT_HASH "85282b9"
 #define GIT_DATE "2021-01-08"
