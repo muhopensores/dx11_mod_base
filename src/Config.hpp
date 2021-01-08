6e14697cd026e62f91ac3e91e91746618599f8a3
Mstislav Kapustka <awesome065@yandex.com>
2021-01-08 23:47:27 +0400

    fix up vs2017 build scripts

--- src/Config.hpp
+++
@@ -1,5 +0,0 @@
-#define LOG_FILENAME "DMC2_framework_log.txt"
-#define LOG_ENTRY "DMC2 ModFramework entry"
-#define CONFIG_FILENAME "DMC2_fw_config.txt"
-#define GIT_HASH "f9404c9"
-#define GIT_DATE "2021-01-08"
